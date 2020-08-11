6924c720576eeb0485252d6615dce996245f3cc8
Sebastian <garciaflorez.sebastian@gmail.com>
2020-08-10 23:27:15 -0500

    Se agrega lupa, se agrega recoger objetos

---
+++ Assets/scripts/pickup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    // Start is called before the first frame update    
+public Transform theDest;
+public CameraLook hcameralook;
+void OnMouseDown() 
+{
+    //GetComponent<BoxCollider>().enabled =  false;
+    GetComponent<Rigidbody>().useGravity = false;
+    this.transform.position = theDest.position;
+    this.transform.parent = GameObject.Find("Destination").transform;
+}
+
+void OnMouseUp() {
+    Debug.Log("hstop");
+    this.transform.parent = null;
+    GetComponent<Rigidbody>().useGravity = true;
+    //GetComponent<BoxCollider>().enabled = true;
+}
+
+void OnTriggerEnter (Collider collider) 
+{
+    Debug.Log("collider");
+    Debug.Log(collider);
+    GameObject objeto1 = GameObject.Find("cilindro1");
+    if (collider.gameObject.name == "fractura") 
+    {
+        
+        GameObject.Find("Main Camera").GetComponent<CameraLook>().enabled = false;
+        GameObject.Find("Player").GetComponent<PlayerMove>().enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        
+    }
+}
+
+}
